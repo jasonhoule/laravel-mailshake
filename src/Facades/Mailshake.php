e9584fe9efae7e0fc3938ed6f8e2194b481a91e6
Jason Houle <jhoule@connecticutchildrens.org>
2020-01-17 16:32:33 -0500

    Updated the Mailshake facade to call Me

--- src/Facades/Mailshake.php
+++ src/Facades/Mailshake.php
@@ -3,10 +3,15 @@
 namespace Jhoule\Mailshake\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Jhoule\Mailshake\Me;
 
 class Mailshake extends Facade
 {
+
     public static function me()
     {
+        $me = new Me();
+        return $me->get();
     }
+
 }
